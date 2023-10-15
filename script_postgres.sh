aa2ede12befc7d276f31f1f2906836f4592ddae7
LéoMermet <leo.mermet91@gmail.com>
2023-10-15 16:43:17 +0200

    one line script for terraform

--- script_postgres.sh
+++ script_postgres.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get install postgresql postgresql-contrib
-sudo systemctl start postgresql
-sudo -u postgres psql -c "CREATE DATABASE todo_db;"
+sudo apt-get update && sudo apt-get install postgresql postgresql-contrib && sudo systemctl start postgresql && sudo -u postgres psql -c "CREATE DATABASE todo_db;"
