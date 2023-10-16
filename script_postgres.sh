eedaad388582423e8d55a2c4d9b6a571b05e71ed
elim5846 <eric.lim5846@gmail.com>
2023-10-16 22:40:42 +0200

    fix postgres version file

--- script_postgres.sh
+++ script_postgres.sh
@@ -11,8 +11,8 @@ sudo -u postgres psql -c "create database todo_db;"
 sudo -u postgres psql -c "create user docker with encrypted password 'docker';" 
 sudo -u postgres psql -c "grant all privileges on database todo_db to docker;"
 psql -U azureadmin -d todo_db -a -f backend/init.sql
-sudo chmod 777 /etc/postgresql/14/main/postgresql.conf
-sudo echo -e "\nlisten_addresses = '*'\nwal_level = replica\n" >> /etc/postgresql/14/main/postgresql.conf 
-sudo chmod 777 /etc/postgresql/14/main/pg_hba.conf 
-sudo echo -e "\nhost all all 0.0.0.0/0 scram-sha-256\nhost replication replicator 0.0.0.0/0 scram-sha-256" >> /etc/postgresql/14/main/pg_hba.conf
+sudo chmod 777 /etc/postgresql/16/main/postgresql.conf
+sudo echo -e "\nlisten_addresses = '*'\nwal_level = replica\n" >> /etc/postgresql/16/main/postgresql.conf 
+sudo chmod 777 /etc/postgresql/16/main/pg_hba.conf 
+sudo echo -e "\nhost all all 0.0.0.0/0 scram-sha-256\nhost replication replicator 0.0.0.0/0 scram-sha-256" >> /etc/postgresql/16/main/pg_hba.conf
 sudo systemctl restart postgresql
--- script_postgres_replicate.sh
+++ script_postgres_replicate.sh
@@ -5,9 +5,9 @@ wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-
 sudo apt-get update 
 sudo apt-get install -y postgresql postgresql-contrib
 sudo systemctl stop postgresql
-sudo chmod 777 /etc/postgresql/14/main/postgresql.conf 
-sudo echo -e "\nhot_standby = on\n" >> /etc/postgresql/14/main/postgresql.conf 
-sudo su - postgres -c "cp -R /var/lib/postgresql/14/main /var/lib/postgresql/14/main_bak"
-sudo su - postgres -c "rm -rf /var/lib/postgresql/14/main/*"
-echo "replicator" | sudo -S su - postgres -c "pg_basebackup -h $MAIN_DB_IP -D /var/lib/postgresql/14/main -U replicator -P -v -R"
+sudo chmod 777 /etc/postgresql/16/main/postgresql.conf 
+sudo echo -e "\nhot_standby = on\n" >> /etc/postgresql/16/main/postgresql.conf 
+sudo su - postgres -c "cp -R /var/lib/postgresql/16/main /var/lib/postgresql/16/main_bak"
+sudo su - postgres -c "rm -rf /var/lib/postgresql/16/main/*"
+echo "replicator" | sudo -S su - postgres -c "pg_basebackup -h $MAIN_DB_IP -D /var/lib/postgresql/16/main -U replicator -P -v -R"
 sudo systemctl start postgresql
