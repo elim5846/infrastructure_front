f1570675a63ab3c8a2a895f14b1ce0d9da544dab
elim5846 <eric.lim5846@gmail.com>
2023-10-16 16:47:41 +0200

    adding y option to command

--- script_postgres.sh
+++ script_postgres.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get update && sudo apt-get install postgresql postgresql-contrib
+sudo apt-get update && sudo apt-get install -y postgresql postgresql-contrib
 sudo systemctl start postgresql
 sudo -u postgres psql -c "create user azureadmin superuser" 
 sudo -u postgres psql -c "create database todo_db;"
